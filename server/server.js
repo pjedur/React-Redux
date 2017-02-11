f2fbba00998a94e5e99e622814804dca6f25ed09
Petur Halldorsson <Petur Halldorsson>
2017-02-11 01:16:23 +0000

    minor

--- server/server.js
+++ server/server.js
@@ -1,27 +1,27 @@
-var config = require('../webpack.config')
-var express = require('express');
-var path = require('path');
-var webpack = require('webpack')
-var webpackDevMiddleware = require('webpack-dev-middleware')
-var webpackHotMiddleware = require('webpack-hot-middleware')
+const express = require('express');
+const path = require('path');
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
 
-var app = express();
-var port = 3000;
+const app = express();
+const port = 3000;
+const config = require('../webpack.config');
 
-var compiler = webpack(config);
+const compiler = webpack(config);
 app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
 app.use(webpackHotMiddleware(compiler));
 
-app.use(express.static('./dist'));
+app.use(express.static('./static'));
 
-app.get("/", function(req, res) {
+app.get("/", (req, res) => {
   res.sendFile(path.resolve('client/index.html'));
 });
 
-app.listen(port, function(error) {
+app.listen(port, (error) => {
   if (error) {
     console.error(error)
   } else {
-    console.log("Express server listening on port", port);
+    console.log("Listening on port: ", port);
   }
 });
